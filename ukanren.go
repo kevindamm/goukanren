6ec07a949a8072e791b1a5f5b1fc2e4b0a8192de
Kevin Damm <github@kevindamm.com>
2025-06-17 22:37:27 -0700

    annotations on the succinct (scheme-based) representation

--- ukanren.go
+++ ukanren.go
@@ -24,17 +24,23 @@ package ukanren
 /* A micro-Kanren in go, based on the following implementation found in [1]
 in Scheme, by the original authors:
 
+Variables are represented as vectors (we'll represent them as refs)
+
 (define (var c) (vector c))
 (define (var? x) (vector? x))
 (define (var=? x1 x2)
   (= (vector-ref x1 0) (vector-ref x2 0)))
 
+There is no check for circularities when searching for a variable's value in a substitution
+
 (define (walk u s)
 	(let ((pr (and (var? u) (assp (λ (v) (var=? u v)) s))))
 		(if pr (walk (cdr pr) s) u)))
 
-(define (ext - s x v s)
+(define (ext-s x v s)
 	`((,x . ,v) . ,s))
+
+The first of the goal constructors, ≡, returns a goal if the two arguments unify.
 
 (define (≡ u v)
 	(λ_g (s/c)
@@ -43,6 +49,8 @@ in Scheme, by the original authors:
 
 (define (unit s/c) (cons s/c mzero))
 (define mzero '())
+
+Basic unification, the core of any logic programming language.
 
 (define (unify u v s)
 	(let ((u (walk u s)) (v (walk v s)))
@@ -55,21 +63,29 @@ in Scheme, by the original authors:
 				 (and s (unify (cdr u) (cdr v) s))))
 			(else (and (eqv? u v) s)))))
 
+Creates a new variable, and is also a goal constructor.
+
 (define (call/fresh f)
 	(λ_g (s/c)
 		(let ((c (cdr s/c)))
 			((f (var c)) `(,(car s/c) . ,(+ c 1))))))
+
+Disjunction and Conjunction are goal constructors that combine goals.
 
 (define (disj g1 g2)
 	(λ_g (s/c) (mplus (g1 s/c) (g2 s/c))))
 (define (conj g1 g2)
 	(λ_g (s/c) (bind (g1 s/c) g2)))
 
+Merges two streams
+
 (define (mplus $1 $2)
 	(cond
 		((null? $1) $2)
 		((procedure? $1) (λ_$ () (mplus $2 ($1))))
 		(else (cons (car $1) (mplus (cdr $1) $2)))))
+
+Used in `conj` to apply a goal from one stream to all goals in a second stream.
 
 (define (bind $ g)
 	(cond
